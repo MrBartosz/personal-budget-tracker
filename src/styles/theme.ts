f11312a11c3a8fe1fc594521222b00dc13933a35
MrBartosz <maksimowski97@gmail.com>
2023-11-29 15:20:06 +0100

    create-setup-project-structure

---
+++ src/styles/theme.ts
@@ -0,0 +1,26 @@
+export const theme = {
+  colors: {
+    primary: '#18284E',
+    pmedium: '#465371',
+    plight: '#747E95',
+
+    secondary: '#686868',
+    smedium: '#D7D7D7',
+    slight: '#F6F6F6',
+
+    accent: '#37A9B0',
+    amedium: '#87CBD0',
+    alight: '#C8DFE1',
+
+    blue: '#1951A0',
+    bmedium: '#5385BC',
+    blight: '#9DB9D9',
+
+    brown: '#5B2F26',
+    ecru: '#FFFCE6',
+    black: '#000000',
+
+    neutral: '#FFFFFF',
+    error: '#ff0000',
+  },
+}
